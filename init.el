7d64dbae1327b2f620bd4c7390f0952bc4d76e19
Steven Lai <stevenylai@gmail.com>
2017-06-17 00:51:40 +0800

    Update for pair quotes

--- init.el
+++ init.el
@@ -1,3 +1,24 @@
+(add-to-list 'load-path "~/.emacs.d/custom")
+;; Font size
+(set-face-attribute 'default nil :height 150)
+(set-default-font "Monospace-12")
+(global-set-key (kbd "<C-mouse-4>") 'text-scale-decrease)
+(global-set-key (kbd "<C-mouse-5>") 'text-scale-increase)
+
+;; Add Autofill mode to mode hooks.
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+;; Show line number in the mode line.
+(line-number-mode 1)
+;; Show column number in the mode line.
+(column-number-mode 1)
+;; Set auto pair
+(setq skeleton-pair t)
+(global-set-key "(" 'skeleton-pair-insert-maybe)
+(global-set-key "[" 'skeleton-pair-insert-maybe)
+(global-set-key "{" 'skeleton-pair-insert-maybe)
+(global-set-key "\"" 'skeleton-pair-insert-maybe)
+(global-set-key "'" 'skeleton-pair-insert-maybe)
+
 (require 'package)
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
@@ -13,8 +34,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(add-to-list 'load-path "~/.emacs.d/custom")
-
 (require 'setup-general)
 (if (version< emacs-version "24.4")
     (require 'setup-ivy-counsel)
@@ -24,7 +43,42 @@
 (require 'setup-cedet)
 (require 'setup-editing)
 
+;;Column marker
+(require 'column-marker)
+(add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 80)))
+(add-hook 'c-mode-hook (lambda () (interactive) (column-marker-1 80)))
+(add-hook 'c++-mode-hook (lambda () (interactive) (column-marker-1 80)))
+;;Auto-complete
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
 
+;; Automatically remove trailing whitespace when file is saved.
+(add-hook 'python-mode-hook
+      (lambda()
+        (add-hook 'local-write-file-hooks
+              '(lambda()
+                 (save-excursion
+                   (delete-trailing-whitespace))))))
+(add-hook 'c-mode-hook
+      (lambda()
+        (add-hook 'local-write-file-hooks
+              '(lambda()
+                 (save-excursion
+                   (delete-trailing-whitespace))))))
+(add-hook 'c++-mode-hook
+      (lambda()
+        (add-hook 'local-write-file-hooks
+              '(lambda()
+                 (save-excursion
+                   (delete-trailing-whitespace))))))
+(add-hook 'js-mode-hook
+      (lambda()
+        (add-hook 'local-write-file-hooks
+              '(lambda()
+                 (save-excursion
+                   (delete-trailing-whitespace))))))
+(setq-default indent-tabs-mode nil)
 
 ;; function-args
 ;; (require 'function-args)
